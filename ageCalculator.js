603965ed0ed831c125dafe142b0c9237881ba16c
vagrant <jhanvi.213@gmail.com>
2021-05-19 21:21:41 +0000

    js files

---
+++ ageCalculator.js
@@ -0,0 +1,9 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+    let age = currentYear - yearOfBirth;
+    let message = name + ' is ' + age + ' years old.';
+    return message;
+
+}
+console.log(ageCalculator('Suzie', 1983, 2015));
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
